f8e67f6c3b94180d72e0d0608765f032846bd4a4
thiago-br-dev <thiago_br_dev@outlook.com>
2014-08-27 16:40:27 -0300

    Painel de Administrativo

--- src/view/Administrador.java
+++ src/view/Administrador.java
@@ -2,42 +2,74 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Administrador extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
+
 		EventQueue.invokeLater(new Runnable() {
+
 			public void run() {
+
 				try {
+
+					UIManager
+							.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+
 					Administrador frame = new Administrador();
 					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+
 				}
+
+				catch (Exception e) {
+
+					e.printStackTrace();
+
+				}
+
 			}
+
 		});
+
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public Administrador() {
+
+		setTitle("BONANZA SUPERMECADOS - T\u00C1 BEM AQUI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1000, 700);
+		setExtendedState(MAXIMIZED_BOTH);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnPrincipal = new JMenu("Principal");
+		menuBar.add(mnPrincipal);
+
+		JMenu mnRelatrios = new JMenu("Relat\u00F3rios");
+		menuBar.add(mnRelatrios);
+		
+		JMenuItem mntmGerarNovo = new JMenuItem("Gerar Novo");
+		mnRelatrios.add(mntmGerarNovo);
+
+		JMenu mnConfigurao = new JMenu("Configura\u00E7\u00E3o");
+		menuBar.add(mnConfigurao);
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+
 	}
 
 }
