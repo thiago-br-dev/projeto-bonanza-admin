a02341f9a8fee9634069f8c1418300fd0ae78782
PauloLima <paulo.lima.dev@gmail.com>
2014-08-28 16:09:54 -0300

    modelos
    classes modelos

---
+++ src/models/Caixa.java
@@ -0,0 +1,28 @@
+package models;
+
+public class Caixa {
+
+	private int id;
+	private String caixa;
+	private String atendente;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCaixa() {
+		return caixa;
+	}
+	public void setCaixa(String caixa) {
+		this.caixa = caixa;
+	}
+	public String getAtendente() {
+		return atendente;
+	}
+	public void setAtendente(String atendente) {
+		this.atendente = atendente;
+	}
+	
+	
+}
